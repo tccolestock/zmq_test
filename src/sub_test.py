91d35c15c21d679fbf5b245048a7359a5bbb7ad5
Thomas Colestock <tccolestock@gmail.com>
2016-08-02 23:23:47 -0400

    Class with Method Testing

--- src/sub_test.py
+++ src/sub_test.py
@@ -6,12 +6,40 @@ from std_msgs.msg import String, Float32, UInt8
 from turtlesim.msg import Color
 import zmq
 
-def callback(data):
+
+class fobar(object):
+    """docstring for fobar"""
+    def __init__(self, ):
+        self.r = []
+        self.g = 0
+        self.b = 0
+
+
+
+
+
+def callback(data, c):
+    print("First in method")
+    print(c.r)
+
     value = data.r
+    c.r.append(value)
+    print("Second in method")
+    print(c.r)
+    fun2()
     # pub.publish(value)
-    print("Value: %f" % value)
+    # print("Value: %f" % value)
+
+def fun2():
+    print("In other function")
+    print(fo.r)
+    # x = fo.r
+    # print("x: %s" % x)
+    r = fo.r
+    r.append(17)
 
 if __name__ == '__main__':
     rospy.init_node("sub_test", anonymous=True)
-    rospy.Subscriber("/turtle1/color_sensor", Color, callback)
+    fo = fobar()
+    rospy.Subscriber("/turtle1/color_sensor", Color, callback, fo)
     rospy.spin()
