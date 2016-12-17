5829ab2bc4a989dc051f6b4ebffe52ab9c5281ed
tccolestock <tccolestock@gmail.com>
2016-12-17 14:41:48 -0500

    Fixed publisher only running once. spin()->while not shutdown

--- src/pub.py
+++ src/pub.py
@@ -12,6 +12,7 @@ from std_msgs.msg import String, Float32, UInt8
 rospy.init_node("pub_test", anonymous=True)
 pub = rospy.Publisher("test3", String, queue_size=10)
 time.sleep(2)
+rate_handle = rospy.Rate(10)  # hz
 
 
 def main():
@@ -19,5 +20,6 @@ def main():
     pub.publish(x)
 
 if __name__ == '__main__':
-    main()
-    rospy.spin()
+    while not rospy.is_shutdown():
+        main()
+        rate_handle.sleep()
